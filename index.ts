18ef74b9aedd866b90f4d2921311bf9c358b8dc0
AbhinayAmbati <abhinayambati4@gmail.com>
2025-11-05 22:31:51 +0530

    Added connection to mysql database and fetched user data and displayed in html page

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 import figlet from "figlet";
 import index from './index.html'
+import {SQL} from "bun"
+
+const mysql = new SQL("mysql://root:abhinay@28aa@localhost:3306/student");
+const mysqlResults = await mysql`
+  SELECT * FROM student;
+`;
 
 const server = Bun.serve({
 	port: 3000,
@@ -13,6 +19,11 @@ const server = Bun.serve({
             const user = { name: "Abhinay" };
             return new Response(JSON.stringify(user), {
             headers: { "Content-Type": "application/json" },
+            });
+        },
+        "/userdata": () => {
+            return new Response(JSON.stringify(mysqlResults),{
+                headers: {"Content-Type": "application/json"},
             });
         }
 	}
