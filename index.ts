650205342ce42a80e48dae8cdcc351a9e58b7820
AbhinayAmbati <abhinayambati4@gmail.com>
2025-11-05 22:16:27 +0530

    Add simple user fetching functionality and update HTML structure

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ const server = Bun.serve({
         "/figlet" : () => {
             const body = figlet.textSync('Bun!');
             return new Response(body);
+        },
+        "/user": () => {
+            const user = { name: "Abhinay" };
+            return new Response(JSON.stringify(user), {
+            headers: { "Content-Type": "application/json" },
+            });
         }
 	}
 });
